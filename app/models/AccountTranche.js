128a98fb91190a1a820f5f9245ff0dc1c8a3ba61
AdityaDubey1811 <adityad@leewayhertz.com>
2022-03-11 18:45:02 +0530

    /api/accountByTranche created

---
+++ app/models/AccountTranche.js
@@ -0,0 +1,22 @@
+let mongoose = require("mongoose");
+let Schema = mongoose.Schema;
+
+const AccountTrancheSchema = new Schema({
+    balanceFrom: { type: Number, default: 0 },
+    balanceTo: { type: Number, default: 0 },
+    accounts: { type: Number, default: 0 },
+    usdBalance: { type: Number, default: 0 },
+    xdcBalance: { type: Number, default: 0 },
+});
+
+AccountTrancheSchema.method({
+  saveData: async function () {
+    return await this.save();
+  },
+});
+AccountTrancheSchema.static({
+    getAccountByTranche: function () {
+    return this.find({});
+  },
+});
+module.exports = mongoose.model("xin-tranche", AccountTrancheSchema);
